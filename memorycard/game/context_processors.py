399402090240169498d39676a28ab67886e49dfa
Artur <bambukov17@gmail.com>
2024-08-24 22:30:18 +0300

    fixed bugs about not authorized users regarding menu 'fast start'

--- memorycard/game/context_processors.py
+++ memorycard/game/context_processors.py
@@ -1,7 +1,10 @@
+import sys
 sys.path.append('../')
 from users.models import *
 sys.path.remove('../')
 
 def random_bot(request):
     bot = CustomUsers.objects.filter(is_bot=True).order_by("?").first()
+    if not bot:
+        bot = {"slug":"no_one_bot_is_alive"}
     return {'random_bot': bot}
